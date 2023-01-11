23559dfba4c296f03a4b57959e9853744cc35f4a
Kevin Yu <112964418+kevin10261@users.noreply.github.com>
2023-01-11 10:05:40 -0700

    Delete Daily-Word directory

--- Daily-Word/README.md
+++
@@ -1,6 +0,0 @@
-# Daily-Word
-Takes a excel spread sheet of translated words (can be any language) and displays them daily for words to learn in a new language.
-
-We used skills like Flask, and openpyxl to create the website
-
-made by Kevin Yu, Andrew Chan
--- Daily-Word/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from view import views
-
-app = Flask(__name__)  # Initializing app
-app.register_blueprint(views, url_prefix="/")
-
-if __name__ == '__main__':
-    # debug=True allows website to refresh when we change things automatically
-    app.run(debug=True)
